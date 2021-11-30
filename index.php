bc930e09db6c15461033f2d9085473835d765f1f
josepuma02020 <jose.pumarejo02@gmail.com>
2021-11-30 12:42:50 -0500

    se agrego orden al index (main) y se agregaron ayudar para en buscador robots y descripcion

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <title>BIENVENIDO</title>
         <meta name="viewport" content="width=device-width, initial-scale=1 ,user-scalable=no">
         <meta charset="utf-8">
+        <meta name="description" content="Software para manejar y optimizar el proceso de tus rutas de cobro y prestamos.">
+        <meta name="robots" context="index,follow"> 
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
         <link rel="stylesheet"  type="text/css"  href="diseno/defecto.css" >
         <link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css" >
@@ -26,28 +28,29 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="diseno/defecto.css" />
     </head>
     <body >
+        <main>
         <br>
         <br><br>
         <br>
-        <div class="container"  >
-            <center>                            
-                <h1 class="textoadapatable"><b>ORION</b></h1>                           
-                <img src="../../ORION/orion.png" style="width: 8%;position:absolute ;left: 630px" align="middle">
-                <br>
-                <br>
-                <br>
-                <br>
-                <h4 for="usuario" class="textoadapatable"><b>Usuario:</b></h4>
-                <input type="text" style="width: 40%"  class=" form-control " id="usuario" placeholder="Ingrese su Usuario" name="usuario">                                                       
-                <h4 for="usuario" class="textoadapatable"><b>Clave:</b></h4>
-                <input type="password" style="width: 40%"  class="form-control " id="clave" placeholder="Ingrese su Contraseña" name="clave"> 
-                <br>
-                <button type="submit" id="iniciar" class="btn btn-default">Ingresar</button>
-            </center>
-        </div>
+            <div class="container"  >
+                <center>                            
+                    <h1 class="textoadapatable"><b>ORION</b></h1>                           
+                    <img src="../../ORION/orion.png" style="width: 8%;position:absolute ;left: 630px" align="middle">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <h4 for="usuario" class="textoadapatable"><b>Usuario:</b></h4>
+                    <input type="text" style="width: 40%"  class=" form-control " id="usuario" placeholder="Ingrese su Usuario" name="usuario">                                                       
+                    <h4 for="usuario" class="textoadapatable"><b>Clave:</b></h4>
+                    <input type="password" style="width: 40%"  class="form-control " id="clave" placeholder="Ingrese su Contraseña" name="clave"> 
+                    <br>
+                    <button type="submit" id="iniciar" class="btn btn-default">Ingresar</button>
+                 </center>
+            </div>
+        </main>
     </body>
     <footer>
         <center>
