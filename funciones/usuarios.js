20bce50da758c9cb03304517167525189337a65e
josepuma02020 <jose.pumarejo02@gmail.com>
2021-12-12 00:08:49 -0500

    se agregar funciones separadas js para usuarios

---
+++ funciones/usuarios.js
@@ -0,0 +1,55 @@
+function editarusuario(id, cedula, nombre, apellido, direccion, telefono, usuario, clave, rol) {
+    cadenau = "id=" + id + "&cedula=" + cedula + "&nombre=" + nombre + "&rol=" + rol + "&apellido=" + apellido + "&direccion=" + direccion + "&telefono=" + telefono + "&clave=" + clave + "&usuario=" + usuario;
+    $.ajax({
+        type: "POST",
+        url: "usuarios/editarusuario.php",
+        data: cadenau,
+        success: function(r) {
+            if (r == 1) {
+                console.log(r);
+                debugger
+            } else {
+                console.log(r);
+                debugger;
+            }
+        }
+    });
+}
+
+function agregarusuario(cedula, nombre, apellido, direccion, telefono, clave, usuario, rol) {
+    cadenau = "cedula=" + cedula + "&nombre=" + nombre + "&rol=" + rol + "&apellido=" + apellido + "&direccion=" + direccion + "&telefono=" + telefono + "&clave=" + clave + "&usuario=" + usuario;
+    $.ajax({
+        type: "POST",
+        url: "usuarios/agregarusuario.php",
+        data: cadenau,
+        success: function(r) {
+            if (r == 1) {
+                console.log(r);
+                debugger
+            } else {
+                console.log(r);
+                debugger;
+            }
+        }
+    });
+}
+
+function agregardatosusuario(id) {
+    $.ajax({
+        type: "POST",
+        data: "id=" + id,
+        url: "usuarios/datosusuario.php",
+        success: function(r) {
+            dato = jQuery.parseJSON(r);
+            $('#cedulau').val(dato['cedula']);
+            $('#idu').val(dato['id']);
+            $('#nombreu').val(dato['nombre']);
+            $('#apellidou').val(dato['apellido']);
+            $('#direccionu').val(dato['direccion']);
+            $('#telefonou').val(dato['telefono']);
+            $('#rolactual').val(dato['Rol']);
+            $('#usuariou').val(dato['usuario']);
+            $('#ultima').val(dato['ultconexion']);
+        }
+    });
+}
