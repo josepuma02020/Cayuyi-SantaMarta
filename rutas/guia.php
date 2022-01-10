38958791ff8224548d1eb26d53764490a58eead5
josepuma02020 <jose.pumarejo02@gmail.com>
2022-01-10 17:56:56 -0500

    para mostrar los dias para el vencimiento del prestamo en la guia

--- rutas/guia.php
+++ rutas/guia.php
@@ -11,6 +11,8 @@ $filasnombreruta = mysqli_fetch_array($querynombreruta);
 $nombreruta = $filasnombreruta['ruta'];
 $consultaruta = "select a.*,b.nombre,b.apellido from prestamos a inner join clientes b on a.cliente=b.id_cliente  where a.ruta = $ruta and a.abonado < a.valorapagar";
 $query = mysqli_query($link, $consultaruta) or die($consultaruta);
+$fecha_actual = date("Y-m-j");
+$fecha_actual = date_create($fecha_actual);
 class PDF extends FPDF
 {
     // Cabecera de página
@@ -45,7 +47,7 @@ class PDF extends FPDF
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->Cell(50, 10, 'Ruta :' . $nombreruta, 0    , 1, 'C', 0);
+$pdf->Cell(50, 10, 'Ruta :' . $nombreruta, 0, 1, 'C', 0);
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(50, 10, 'Nombre', 1, 0, 'C', 0);
 $pdf->Cell(30, 10, 'Fecha P', 1, 0, 'C', 0);
@@ -56,12 +58,33 @@ $pdf->Cell(20, 10, 'DA', 1, 0, 'C', 0);
 $pdf->Cell(20, 10, 'DV', 1, 1, 'C', 0);
 $pdf->SetFont('Arial', '', 12);
 while ($filas1 = mysqli_fetch_array($query)) {
+
+    //dias para vencimiento de prestamo activo
+
+    $diasprestamoactivo = $filas1['dias_prestamo'];
+    $fechaprestamoactivo = $filas1['fecha'];
+    $fechaprestamoactivo = date_create($fechaprestamoactivo);
+    date_add($fechaprestamoactivo, date_interval_create_from_date_string("$diasprestamoactivo days"));
+    $fechafinprestamo = date_format($fechaprestamoactivo, "d-m-Y");
+    $diff = $fecha_actual->diff($fechaprestamoactivo);
+    $vencimiento = $diff->days;
+    $fechafinprestamo = date_create($fechafinprestamo);
+    if ($fechafinprestamo > $fecha_actual) {
+        $vencimiento = $vencimiento * -1;
+    }
+    $vencimiento . '-';
+
+    if ($vencimiento > 0) {
+        $class = 'input-disabled-vencido';
+    } else {
+        $class = 'input-disabled-normal';
+    }
     $pdf->Cell(50, 10, $filas1['nombre'] . ' ' . $filas1['apellido'], 1, 0, 'C', 0);
     $pdf->Cell(30, 10, $filas1['fecha'], 1, 0, 'C', 0);
     $pdf->Cell(20, 10, $filas1['valor_prestamo'], 1, 0, 'C', 0);
     $pdf->Cell(20, 10, $filas1['valorapagar'] - $filas1['abonado'], 1, 0, 'C', 0);
     $pdf->Cell(20, 10, '', 1, 0, 'C', 0);
     $pdf->Cell(20, 10, $filas1['dias_atraso'], 1, 0, 'C', 0);
-    $pdf->Cell(20, 10, '', 1, 1, 'C', 0);
+    $pdf->Cell(20, 10, $vencimiento, 1, 1, 'C', 0);
 }
 $pdf->Output();
