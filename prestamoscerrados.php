203946e5f4f98f5baec33cf439caf748d7a9bc90
josepuma02020 <jose.pumarejo02@gmail.com>
2022-03-25 14:58:10 -0500

    implementacion de busqueda por fechas en prestamos cerrados

--- prestamoscerrados.php
+++ prestamoscerrados.php
@@ -3,6 +3,16 @@ session_start();
 if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
     include_once('conexion/conexion.php');
     setlocale(LC_ALL, "es_CO");
+    if (isset($_GET['desde'])) {
+        $desde = $_GET['desde'];
+    } else {
+        $desde = date("Y-m-01");
+    }
+    if (isset($_GET['hasta'])) {
+        $hasta = $_GET['hasta'];
+    } else {
+        $hasta = date("Y-m-d");
+    }
     $fechahoyval = date("Y") . '-' . date("m") . '-' . date("j");
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
@@ -50,16 +60,19 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
             <section class="titulo-pagina">
                 <h1>Préstamos cancelados</h1>
             </section>
-            <span class="btn btn-primary boton-parametro" data-toggle="modal" data-target="#nuevoprestamo">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z" />
-                    <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" />
-                    <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z" />
-                    <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
-                </svg> Nuevo Prestamo<span class="fa fa-plus-circle"></span>
-                <br>
-            </span>
-
+            <div class="form-group col-sm-3">
+                <h4>Desde:</h4>
+                <input class="form-control input-sm" type="date" id="desde" name="desde" value="<?php echo $desde ?>">
+            </div>
+            <div class="form-group col-sm-3">
+                <h4>Hasta:</h4>
+                <input class="form-control input-sm" type="date" id="hasta" name="hasta" value="<?php echo $hasta ?>">
+            </div>
+            <button type="button" id="buscar" class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+            </button>
             <div class="modal fade  " id="nuevoprestamo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg ">
                     <div class="modal-content">
@@ -218,18 +231,16 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
                 <THEAD>
                     <tr>
                         <th> Ruta </th>
-                        <th> Nombre </th>
+                        <th> Cliente </th>
                         <th> F.Préstamo</th>
                         <th> V.Prestado </th>
                         <th> V.a pagar </th>
-                        <th> Abonado </th>
-                        <th> Saldo </th>
-                        <th> D.A </th>
+                        <th> F.Cierre </th>
                     </tr>
                 </THEAD>
                 <TBODY>
                     <?php
-                    $consultarutas = "select a.cliente,c.ruta'nombreruta',a.fecha,a.id_prestamo,b.nombre,a.valor_prestamo,valorapagar,abonado,dias_atraso from prestamos a inner join clientes b on a.cliente=b.id_cliente inner join rutas c on c.id_ruta=a.ruta where a.valorapagar <= (a.abonado)";
+                    $consultarutas = "select a.fechacierre,a.cliente,c.ruta'nombreruta',a.fecha,a.id_prestamo,b.nombre,a.valor_prestamo,valorapagar,abonado,dias_atraso from prestamos a inner join clientes b on a.cliente=b.id_cliente inner join rutas c on c.id_ruta=a.ruta where a.valorapagar <= (a.abonado) and a.fechacierre BETWEEN '$desde' and '$hasta'";
                     $query = mysqli_query($link, $consultarutas) or die($consultarutas);
                     while ($filas1 = mysqli_fetch_array($query)) {
                     ?>
@@ -239,17 +250,7 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
                             <TD><?php echo $filas1['fecha']; ?> </TD>
                             <TD><?php echo $filas1['valor_prestamo']; ?> </TD>
                             <TD><?php echo $filas1['valorapagar']; ?> </TD>
-                            <TD><a href="historialcuotas.php?id=<?php echo $filas1['id_prestamo'] ?>"><?php echo $filas1['abonado']; ?></a> </TD>
-                            <TD><?php echo $filas1['valorapagar'] - $filas1['abonado']; ?> </TD>
-                            <?php
-                            if ($filas1['dias_atraso'] > 10) {
-                                $aviso = "EC1A3D";
-                            } else {
-                                $aviso = "";
-                            }
-                            ?>
-                            <TD style="background-color: <?php echo $aviso ?>"><?php echo $filas1['dias_atraso']; ?> </TD>
-
+                            <TD><?php echo $filas1['fechacierre']; ?> </TD>
                         </TR>
                     <?php } ?>
                 </tbody>
@@ -277,6 +278,12 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
+        $('#buscar').click(function() {
+            a = 0;
+            desde = $('#desde').val();
+            hasta = $('#hasta').val();
+            location.href = `prestamoscerrados.php?desde=${desde}&hasta=${hasta}`;
+        });
         $('#nformapago').change(function() {
             dias = $('#diasu').val();
             totalpagar = $('#totalpagaru').val();
