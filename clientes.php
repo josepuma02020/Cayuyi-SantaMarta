77f0c597cdff993c99bb30e1533d8fe413057b8a
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-07-01 19:26:53 -0500

    implementando el historial de clientes

--- clientes.php
+++ clientes.php
@@ -86,6 +86,7 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
                         <THEAD>
                             <tr>
                                 <th> Nombre </th>
+                                <th> Cedula </th>
                                 <th> Telefono </th>
                                 <th> Direccion </th>
                                 <th> Saldo Pendiente </th>
@@ -101,6 +102,7 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
                             ?>
                                 <TR>
                                     <TD><?php echo $filas1['nombre']; ?> </TD>
+                                    <TD><?php echo $filas1['cedula']; ?> </TD>
                                     <TD><?php echo $filas1['telefono']; ?> </TD>
                                     <TD><?php echo $filas1['direccion']; ?> </TD>
                                     <TD><?php
@@ -118,6 +120,14 @@ if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
                                                 <path fill-rule="evenodd" d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                                             </svg>
                                         </button>
+                                        <a href="historialcliente.php?id=<?php echo $filas1['id_cliente'] ?>">
+                                            <button type="button" id="historial" class="btn btn-primary" data-toggle="modal" data-target="#editar">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                                                    <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                                                </svg>
+                                            </button>
+                                        </a>
                                     </TD>
                                 </TR>
                             <?php } ?>
---
+++ historialcliente.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+if ($_SESSION['usuario'] && $_SESSION['Rol'] == 1) {
+    include_once('conexion/conexion.php');
+    setlocale(LC_ALL, "es_CO");
+    date_default_timezone_set('America/Bogota');
+    $fechahoyval = date("Y") . '-' . date("m") . '-' . date("j");
+    $id = $_GET['id'];
+
+    //consultacliente
+    $consulta = "select * from clientes where id_cliente = $id";
+    $querycliente = mysqli_query($link, $consulta) or die($consulta);
+    $cliente = mysqli_fetch_array($querycliente);
+?>
+    <HTML>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css" />
+        <link rel="stylesheet" href="./diseno/defecto/desktop.css" />
+        <link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css" />
+        <link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/default.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <SCRIPT src="librerias/jquery-3.5.1.min.js"></script>
+        <SCRIPT src="librerias/alertify/alertify.js"></script>
+        <SCRIPT lang="javascript" type="text/javascript" src="./prestamos/prestamos.js"></script>
+        <script src="librerias/bootstrap/js/bootstrap.js"></script>
+        <link type="text/css" href="librerias/jquery-ui-1.12.1.custom/jquery-ui.structure.min.css" rel="Stylesheet" />
+        <link rel="shortcut  icon" href="imagenes/logop.png" type="image/x-icon" />
+    </head>
+
+    <body>
+        <header>
+            <?php
+            include_once($_SESSION['menu']);
+            ?>
+        </header>
+        <main class=" container container-md">
+
+            <section class="titulo-pagina">
+                <h1>Historial de Cliente</h1>
+            </section>
+            <div class="form-group col-sm-3">
+                <h4>Cliente:</h4>
+                <input class="form-control input-sm" type="text" disabled id="desde" name="desde" value="<?php echo $cliente['nombre'] ?>">
+            </div>
+
+            <TABLE class="table table-striped  table-responsive-lg" id="tablahistorialcliente">
+                <THEAD>
+                    <tr>
+                        <th> F.Préstamo</th>
+                        <th> V.Prestado </th>
+                        <th> V.a pagar </th>
+                        <th> Saldo </th>
+                        <th> F.Cierre </th>
+                        <th> Detalles </th>
+                    </tr>
+                </THEAD>
+                <TBODY>
+                    <?php
+                    $consultarutas = "select a.* ,b.nombre  from prestamos a inner join clientes b on b.id_cliente=a.cliente where cliente = $id";
+                    $query = mysqli_query($link, $consultarutas) or die($consultarutas);
+                    while ($filas1 = mysqli_fetch_array($query)) {
+                    ?>
+                        <TR>
+                            <TD><?php echo $filas1['fecha']; ?> </TD>
+                            <TD><?php echo number_format($filas1['valor_prestamo']); ?> </TD>
+                            <TD><?php echo number_format($filas1['valorapagar']); ?> </TD>
+                            <TD><?php echo number_format($filas1['valorapagar'] - $filas1['abonado']); ?> </TD>
+                            <TD><?php echo $filas1['fechacierre']; ?> </TD>
+                            <TD>
+                                <SCRIPT lang="javascript" type="text/javascript" src="funciones/funciones.js"></script>
+                                <button onclick="obtenerdatosprestamo(<?php echo $filas1['id_prestamo'] ?>)" type="button" id="actualiza" class="btn btn-primary" data-toggle="modal" data-target="#editar">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                    </svg>
+                                </button>
+                            </TD>
+                        </TR>
+                    <?php } ?>
+                </tbody>
+
+            </table>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+
+    </HTML>
+
+<?php
+} else {
+    header('Location: ' . "usuarios/cerrarsesion.php");
+}
+?>
+<script type="text/javascript" src="librerias/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+<script type="text/javascript" src="librerias/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        tabla = $('#tablahistorialcliente').DataTable({
+            language: {
+                url: '../vendor/datatables/es-ar.json',
+                lengthMenu: "Mostrar _MENU_ Registros",
+                loadingRecords: "Cargando...",
+                search: "Buscar:",
+                info: "Mostrando lista de Préstamos",
+                zeroRecords: "Sin Resultados",
+                paginate: {
+                    first: "Primera pagina",
+                    previous: "Anterior",
+                    next: "Siguiente",
+                    last: "Ultima"
+                },
+            }
+        });
+    });
+</script>
