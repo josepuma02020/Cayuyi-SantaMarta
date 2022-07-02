77c81829b0b9225bd5cd7b506f3d9188a872e663
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-07-02 11:15:49 -0500

    modificando que solo puede eliminar registros conrables el admin

--- contabilidad.php
+++ contabilidad.php
@@ -87,7 +87,14 @@ if ($_SESSION['usuario'] && ($_SESSION['Rol'] == 1 || $_SESSION['Rol'] == 2)) {
                         <th> S </th>
                         <th> Cli. </th>
                         <th> Fecha </th>
-                        <th> Acciones </th>
+                        <?php
+                        if ($_SESSION['Rol'] == 1) {
+                        ?>
+                            <th> Acciones </th>
+                        <?php
+                        }
+                        ?>
+
                     </tr>
                 </THEAD>
                 <TBODY>
@@ -110,14 +117,22 @@ if ($_SESSION['usuario'] && ($_SESSION['Rol'] == 1 || $_SESSION['Rol'] == 2)) {
                             <TD><?php echo number_format($filas1['salientes']); ?> </TD>
                             <TD><?php echo ($filas1['clientes']); ?> </TD>
                             <TD><?php echo ($filas1['fecha']); ?> </TD>
-                            <td>
-                                <button onclick="agregaridcontable(<?php echo $filas1['idhistorial'] ?>)" type="button" id="eeeee" class="btn btn-danger" data-toggle="modal" data-target="#eliminar">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel" viewBox="0 0 16 16">
-                                        <path d="M5.884 6.68a.5.5 0 1 0-.768.64L7.349 10l-2.233 2.68a.5.5 0 0 0 .768.64L8 10.781l2.116 2.54a.5.5 0 0 0 .768-.641L8.651 10l2.233-2.68a.5.5 0 0 0-.768-.64L8 9.219l-2.116-2.54z" />
-                                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
-                                    </svg>
-                                </button>
-                            </td>
+
+                            <?php
+                            if ($_SESSION['Rol'] == 1) {
+                            ?>
+                                <td>
+                                    <button onclick="agregaridcontable(<?php echo $filas1['idhistorial'] ?>)" type="button" id="eeeee" class="btn btn-danger" data-toggle="modal" data-target="#eliminar">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel" viewBox="0 0 16 16">
+                                            <path d="M5.884 6.68a.5.5 0 1 0-.768.64L7.349 10l-2.233 2.68a.5.5 0 0 0 .768.64L8 10.781l2.116 2.54a.5.5 0 0 0 .768-.641L8.651 10l2.233-2.68a.5.5 0 0 0-.768-.64L8 9.219l-2.116-2.54z" />
+                                            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            <?php
+                            }
+                            ?>
+
                         </TR>
                     <?php } ?>
                 </TBODY>
