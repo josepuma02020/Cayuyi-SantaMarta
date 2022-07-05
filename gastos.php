ac2b3d61a51e66f9b1dacd0ab43195d412517269
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-07-05 13:27:15 -0500

    restringiendo la eliminacion de gastos solo para admin

--- gastos.php
+++ gastos.php
@@ -124,7 +124,14 @@ if ($_SESSION['usuario'] && ($_SESSION['Rol'] == 1 || $_SESSION['Rol'] == 2)) {
                             <th> Ruta </th>
                             <th> Valor </th>
                             <th> Fecha </th>
-                            <th> Acciones </th>
+                            <?php
+                            if ($_SESSION['Rol'] == 1) {
+                            ?>
+                                <th> Acciones </th>
+                            <?php
+                            }
+                            ?>
+
                         </tr>
                     </THEAD>
                     <TBODY>
@@ -156,15 +163,22 @@ if ($_SESSION['usuario'] && ($_SESSION['Rol'] == 1 || $_SESSION['Rol'] == 2)) {
                                 <TD><?php echo $filas1['ruta']; ?> </TD>
                                 <TD><?php echo number_format($filas1['valor']); ?> </TD>
                                 <TD><?php echo $filas1['fecha']; ?> </TD>
-                                <TD>
-                                    <SCRIPT lang="javascript" type="text/javascript" src="funciones/funciones.js"></script>
-                                    <button onclick="agregaridgasto(<?php echo $filas1['id_gasto'] ?>)" type="button" id="eliminargasto" class="btn btn-danger" data-toggle="modal" data-target="#eliminar">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-excel" viewBox="0 0 16 16">
-                                            <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
-                                            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-                                        </svg>
-                                    </button>
-                                </TD>
+                                <?php
+                                if ($_SESSION['Rol'] == 1) {
+                                ?>
+                                    <TD>
+                                        <SCRIPT lang="javascript" type="text/javascript" src="funciones/funciones.js"></script>
+                                        <button onclick="agregaridgasto(<?php echo $filas1['id_gasto'] ?>)" type="button" id="eliminargasto" class="btn btn-danger" data-toggle="modal" data-target="#eliminar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-excel" viewBox="0 0 16 16">
+                                                <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
+                                                <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                                            </svg>
+                                        </button>
+                                    </TD>
+                                <?php
+                                }
+                                ?>
+
                             </TR>
                         <?php } ?>
                     </TBODY>
