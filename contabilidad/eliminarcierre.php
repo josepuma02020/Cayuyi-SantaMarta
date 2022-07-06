f132adb12c66e9b68c547ebc439c881f23716878
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-07-06 07:33:56 -0500

    corrigiendo descuento de base al eliminar una liquidacion contabilizada

--- contabilidad/eliminarcierre.php
+++ contabilidad/eliminarcierre.php
@@ -3,6 +3,13 @@ session_start();
 if ($_SESSION['usuario']) {
     include('../conexion/conexion.php');
     $id = $_POST['id'];
+    $consultaefecivo_ = "select efectivo,ruta from revisionesrutas where idhistorial=$id";
+    $queryefectivo = mysqli_query($link, $consultaefecivo_) or die($consultaefecivo_);
+    $filaefectivo = mysqli_fetch_array($queryefectivo);
+
+    $consultadescuentobase = "UPDATE `rutas` SET `base`=base - $filaefectivo[efectivo]  WHERE `id_ruta` = $filaefectivo[ruta]";
+    $queryedescuentobase = mysqli_query($link, $consultadescuentobase) or die($consultadescuentobase);
+
     $consulta = "DELETE FROM `revisionesrutas` WHERE idhistorial = $id";
     $query = mysqli_query($link, $consulta) or die($consulta);
 } else {
