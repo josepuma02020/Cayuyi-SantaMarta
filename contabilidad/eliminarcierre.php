a5256f08b5fe4cdd7211ee580871b33800ee016d
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-07-06 07:48:17 -0500

    corrigiendo el proceso de eliminar cierre

--- contabilidad/eliminarcierre.php
+++ contabilidad/eliminarcierre.php
@@ -3,11 +3,11 @@ session_start();
 if ($_SESSION['usuario']) {
     include('../conexion/conexion.php');
     $id = $_POST['id'];
-    $consultaefecivo_ = "select efectivo,ruta from revisionesrutas where idhistorial=$id";
+    $consultaefecivo_ = "select base,ruta from revisionesrutas where idhistorial=$id";
     $queryefectivo = mysqli_query($link, $consultaefecivo_) or die($consultaefecivo_);
     $filaefectivo = mysqli_fetch_array($queryefectivo);
 
-    $consultadescuentobase = "UPDATE `rutas` SET `base`=base - $filaefectivo[efectivo]  WHERE `id_ruta` = $filaefectivo[ruta]";
+    $consultadescuentobase = "UPDATE `rutas` SET `base`=$filaefectivo[base]  WHERE `id_ruta` = $filaefectivo[ruta]";
     $queryedescuentobase = mysqli_query($link, $consultadescuentobase) or die($consultadescuentobase);
 
     $consulta = "DELETE FROM `revisionesrutas` WHERE idhistorial = $id";
